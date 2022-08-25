730b046ddb46d9f41ed40488752c864af7f3a0c2
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-25 14:34:26 -0300

    Aula: Buscar dados em tabela com relacionamento

--- src/create.ts
+++ src/create.ts
@@ -3,23 +3,43 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.courses.create({
-    data: {
-      name: "Curso de React Native 3",
-      duration: 300,
-      description: "Curso excelente de React Native com Rodrigão",
+  // const result = await prisma.courses.create({
+  //   data: {
+  //     name: "Curso de React Native 3",
+  //     duration: 300,
+  //     description: "Curso excelente de React Native com Rodrigão",
+  //     teacher: {
+  //       connectOrCreate: {
+  //         where: {
+  //           id: 'fafasdf'
+  //         },
+  //         create: {
+  //           name: 'Professor 2'
+  //         }
+  //       }
+  //     }
+  //   },
+  // });
+
+  // const result = await prisma.teachers.create({
+  //   data: {
+  //     name: 'Professor 56'
+  //   }
+  // })
+
+  const result = await prisma.courses.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      duration: true,
       teacher: {
-        connectOrCreate: {
-          where: {
-            id: 'fafasdf'
-          },
-          create: {
-            name: 'Professor 2'
-          }
+        select: {
+          name: true
         }
       }
-    },
-  });
+    }
+  })
 
   console.log(result);
 }
