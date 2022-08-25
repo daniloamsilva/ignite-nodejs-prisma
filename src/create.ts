9252ddc5dcb00dad3d597c893312e30a5d250497
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-25 14:20:57 -0300

    Aula: Inserir dados em tabela com relacionamento

--- src/create.ts
+++ src/create.ts
@@ -5,9 +5,19 @@ const prisma = new PrismaClient();
 async function main() {
   const result = await prisma.courses.create({
     data: {
+      name: "Curso de React Native 3",
       duration: 300,
-      name: "Curso de React Native",
       description: "Curso excelente de React Native com Rodrigão",
+      teacher: {
+        connectOrCreate: {
+          where: {
+            id: 'fafasdf'
+          },
+          create: {
+            name: 'Professor 2'
+          }
+        }
+      }
     },
   });
 
