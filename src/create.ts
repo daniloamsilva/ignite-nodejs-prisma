d7f44928ee581d1f4c6bb6fe37edf235b4247d6f
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-25 23:02:31 -0300

    Aula: Inserindo dados em tabela com relacionamento muito para muitos

--- src/create.ts
+++ src/create.ts
@@ -3,65 +3,44 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const result = await prisma.courses.create({
+  // const result = await prisma.modules.create({
   //   data: {
-  //     name: "Curso de React Native 3",
-  //     duration: 300,
-  //     description: "Curso excelente de React Native com Rodrigão",
-  //     teacher: {
-  //       connectOrCreate: {
-  //         where: {
-  //           id: 'fafasdf'
-  //         },
-  //         create: {
-  //           name: 'Professor 2'
+  //     name: 'Module 01',
+  //     description: 'Descrição do módulo 01'
+  //   }
+  // })
+
+  // const result = await prisma.modules.create({
+  //   data: {
+  //     name: 'Module 02',
+  //     description: 'Descrição do módulo 02',
+  //     CoursesModules: {
+  //       create: {
+  //         course: {
+  //           create: {
+  //             name: 'Curso 02',
+  //             duration: 200,
+  //             description: 'Descrição do curso 02'
+  //           }
   //         }
   //       }
   //     }
-  //   },
+  //   }
   // });
 
-  // const result = await prisma.teachers.create({
-  //   data: {
-  //     name: 'Professor 56'
-  //   }
-  // })
-
-  // const result = await prisma.courses.findMany({
-  //   select: {
-  //     id: true,
-  //     name: true,
-  //     description: true,
-  //     duration: true,
-  //     teacher: {
-  //       select: {
-  //         name: true
-  //       }
-  //     }
-  //   }
-  // })
-
-  // const result = await prisma.authors.create({
-  //   data: {
-  //     name: 'Author 02',
-  //     books: {
-  //       createMany: {
-  //         data: [
-  //           { name: "Book 02" },
-  //           { name: "Book 03" },
-  //         ]
-  //       }
-  //     }
-  //   }
-  // })
-
-  const result = await prisma.books.create({
+  const result = await prisma.coursesModules.create({
     data: {
-      name: 'Book 04',
-      author: {
-        connectOrCreate: {
-          where: { id: '9a183c7c-ada7-48d7-bd25-87029ba941ae' },
-          create: { name: 'Author 666' }
+      course: {
+        create: {
+          name: 'Course 02',
+          duration: 400,
+          description: 'Course 02 description'
+        },
+      },
+      modules: {
+        create: {
+          name: 'Module 03',
+          description: 'Module 03 description'
         }
       }
     }
